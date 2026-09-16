0a0d1230015637474fcde70c770484e9776849fb
RiosGabri <grp@cesar.school>
2026-09-16 09:07:56 -0300

    Implementei um enum público TipoSinistro

---
+++ br/edu/cs/poo/ac/seguro/entidades/TipoSinistro.java
@@ -0,0 +1,33 @@
+package br.edu.cs.poo.ac.seguro.entidades;
+
+public enum TipoSinistro {
+    COLISAO(1,"Colisão"),
+    INCENDIO(2,"Incêndio"),
+    FURTO(3, "Furto"),
+    ENCHENTE(4, "Enchente"),
+    DEPREDACAO(5, "Depredação");
+
+    private int codigo;
+    private String nome;
+    private TipoSinistro(int codigo, String nome) {
+        this.nome = nome;
+        this.codigo = codigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public static TipoSinistro getTipoSinistro(int codigo) {
+        for (TipoSinistro tipo : TipoSinistro.values()) {
+            if (tipo.getCodigo() == codigo) {
+                return tipo;
+            }
+        }
+        return null; 
+    }
+}
