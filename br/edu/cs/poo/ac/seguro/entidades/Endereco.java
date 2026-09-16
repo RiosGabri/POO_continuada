439f05cb6ee5ef95c4b243df287124cbae138411
RiosGabri <grp@cesar.school>
2026-09-16 09:33:17 -0300

    Criei classe Endereco

---
+++ br/edu/cs/poo/ac/seguro/entidades/Endereco.java
@@ -0,0 +1,11 @@
+package br.edu.cs.poo.ac.seguro.entidades;
+
+public class Endereco {
+    private String logradouro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String pais;
+    private String estado;
+    private String cidade;
+}
