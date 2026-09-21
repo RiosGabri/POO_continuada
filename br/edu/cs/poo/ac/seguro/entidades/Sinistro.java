58c855a58115d2402d160cd40a213be34688596f
RiosGabri <grp@cesar.school>
2026-09-21 19:58:58 -0300

    Arquivos .java finais da etapa 1 da Atividade Continuada

---
+++ br/edu/cs/poo/ac/seguro/entidades/Sinistro.java
@@ -0,0 +1,22 @@
+package br.edu.cs.poo.ac.seguro.entidades;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Sinistro {
+
+    private String numero;
+    private Veiculo veiculo;
+    private LocalDateTime dataHoraSinistro;
+    private LocalDateTime dataHoraRegistro;
+    private String usuarioRegistro;
+    private BigDecimal valorSinistro;
+    private TipoSinistro tipo;
+}
